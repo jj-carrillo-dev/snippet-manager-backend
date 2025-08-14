a7e3f4ba40bc9d88422d1caec14c9099cf9d7078
Juan Carrillo <ciclon.357@gmail.com>
2025-08-14 19:20:12 +0200

    feat: Add PostgreSQL database connection with secure credentials, and initial configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the config module available everywhere
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true, // IMPORTANT: use for development only. It automatically creates tables from your entities.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
